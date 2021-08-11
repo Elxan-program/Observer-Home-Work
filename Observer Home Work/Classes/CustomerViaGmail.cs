3a38e03c18240bd539207c5cb81ed4a5d16919cd
Elxan Atayev <elxanatayev.la@gmail.com>
2021-08-11 12:23:20 +0400

    Add project files.

---
+++ Observer Home Work/Classes/CustomerViaGmail.cs
@@ -0,0 +1,21 @@
+﻿using Observer_Home_Work.GmailService;
+using Observer_Home_Work.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer_Home_Work.Classes
+{
+    public class CustomerViaGmail : ICustomer
+    {
+        public string ProductName { get; set; }
+        public string CustomerEmail { get; set; }
+
+        public void GetMessage()
+        {
+            Sending_Gmail.SendMail(CustomerEmail);
+        }
+    }
+}
---
+++ Observer Home Work/Classes/Store.cs
@@ -0,0 +1,21 @@
+﻿using Observer_Home_Work.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer_Home_Work.Classes
+{
+    public class Store
+    {
+        public List<ICustomer> Customers { get; set; }
+        public void Notify()
+        {
+            foreach (var customer in Customers)
+            {
+                customer.GetMessage();
+            }
+        }
+    }
+}
